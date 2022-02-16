af2e7f2dc7cdd051afd6b03e55d64169aa942064
bugsbrog <bugsbrog@gmail.com>
2022-02-16 15:59:43 -0700

    Got migrations file ready to start

--- data/migrations/20220216221339_recipes.js.js
+++ data/migrations/20220216221339_recipes.js.js
@@ -1,8 +1,23 @@
+exports.up = async function(knex) {
+  await knex.schema
+      .createTable('recipes', (table) => {
 
-exports.up = function(knex) {
-  
+      })
+      .createTable('ingredients', (table) => {
+
+      })
+      .createTable('steps', (table) => {
+
+      })
+      .createTable('step_ingredients', (table) => {
+
+      })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema
+      .dropTableIfExists('recipes')
+      .dropTableIfExists('ingredients')
+      .dropTableIfExists('steps')
+      .dropTableIfExists('step_ingredients')
 };
