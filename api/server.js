cb65657ea496a3d9b4706de7fc51fa8f187aa441
bugsbrog <bugsbrog@gmail.com>
2022-02-16 17:37:13 -0700

    Finished route and need to do model. Also added err handling mw

--- api/server.js
+++ api/server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 
-// const recipeRouter = require('./recipes/recipes-router')
+const recipeRouter = require('./recipes/recipes-router')
 
 const server = express()
 
 server.use(express.json())
-// server.use('/api/recipes', recipeRouter)
+server.use('/api/recipes', recipeRouter)
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
 
 module.exports = server
